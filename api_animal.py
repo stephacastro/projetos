7b0c28671e93b567f353c066a8fddb4d5e29e7c4
Stephanie Castro <stephanie.castrosilva@gmail.com>
2024-03-05 20:01:22 -0300

    api simples que retorna dados de animais

---
+++ api_animal.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from typing import Optional
+from uuid import uuid4
+
+app = FastAPI()
+
+class Animal(BaseModel):
+    id: Optional[str]
+    nome: str
+    idade: int
+    sexo: str 
+    cor: str
+
+banco_animal: List[Animal] = []
+
+@app.get("/animais")
+async def listar_animais():
+    return banco_animal
+
+@app.get("/animais/{id}")
+async def id_animal(id: str):
+    for animal in banco_animal:
+        if animal.id == id:
+            return animal
+    return {'ERROR': 'Animal não localizado'}
+
+# endpoint que envia os animais 
+@app.post("/animais")
+async def criar_animal(animal: Animal):
+    animal.id = str(uuid4())
+    banco_animal.append(animal)
+    return {"Cadastro": f'Animal:{animal.nome}, Idade:{animal.idade}, Sexo:{animal.sexo}, Cor:{animal.cor}'}
+
+@app.delete("/animais/{id}")
+async def deletar_animais(id: str):
+    for i, animal in enumerate(banco_animal):
+        if animal.id == id:
+            del banco_animal[i]
+            return {'Mensagem' f' ID {id} deletado com sucesso!'}
+    return {'ERROR': 'ID não localizado'}
+    
